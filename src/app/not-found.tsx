3cdcbeb4f2f59f36bd23ffb44f35d1238d000621
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-08-03 14:52:44 +0300

    Add src/app/not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl">404 - Page Not Found</h1>
+      <p className="mt-4">Sorry, we couldn't find the page you're looking for.</p>
+    </div>
+  );
+};
+
+export default NotFound;
