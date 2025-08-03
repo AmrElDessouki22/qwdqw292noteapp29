badfea39006a8730cf49d28febcb1fa91d13703e
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-08-03 14:52:51 +0300

    Add src/components/ui/Footer.tsx

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-blue-500 text-white p-4 text-center">
+      <p>&copy; 2023 Note App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
